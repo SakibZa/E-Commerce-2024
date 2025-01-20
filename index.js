e7a87d2d2c8188050dd485fd4f4220781c6b0107
sakibhusainzaidi2000@gmail.com <sakibhusainzaidi2000@gmail.com>
2025-01-20 20:57:32 +0530

    changes has been made

--- index.js
+++ index.js
@@ -23,14 +23,6 @@ app.get('/',(req, res)=>{
 //middleware
 app.use(express.json());
 app.use(morgan('dev'));
-// app.use((req, res, next) =>{
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-// })
 app.use(cors());
 
 //routes
